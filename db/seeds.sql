90b2daf649bfa39c27afeed238ac8c22923829c0
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-18 14:18:40 -0700

    setup index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,19 @@
 INSERT INTO department (id, name)
 VALUES (1, "John Smith"),
-       (2, "Dan Jones"),
-       (3, "Shelby Herns"),
-       (4, "David Burn"),
-       (5, "Chel Tan");
+        (2, "Dan Jones"),
+        (3, "Shelby Herns"),
+        (4, "David Burn"),
+        (5, "Chel Tan");
 
 INSERT INTO role (id, title, salary)
 VALUES (1, "Manager", 150000.00),
-       (2, "Enginner" 200000.00),
-       (3, "Intern" 50000.00),
-       (4, "HR" 150000.00);
+        (2, "Enginner" 200000.00),
+        (3, "Intern" 50000.00),
+        (4, "HR" 150000.00);
        
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "John" "Smith", 1, 1),
-       (2, "Dan" "Jones", 2, 2),
-       (3, "Shelby" "Herns", 2),
-       (4, "David" "Burn", 3),
-       (5, "Chel" "Tan",  4);
+        (2, "Dan" "Jones", 2, 2),
+        (3, "Shelby" "Herns", 2),
+        (4, "David" "Burn", 3),
+        (5, "Chel" "Tan",  4);
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const { default: inquirer } = require('inquirer');
+const mysql = require('mysql2');
+
+let employeeObjArray = [];
+
+function startQ() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'startQ',
+            message: 'What would you like to do?',
+            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+        }
+    ]).then(function (userInput) {
+        switch (userInput.startQ) {
+            case "view all department": allDepartments();
+                break;
+
+            case "view all roles": allRoles();
+                break;
+
+            case "view all employees": allEmployees();
+                break;
+
+            case "add a department": addDepartment();
+                break;
+
+            case "add a role": addRole();
+                break;
+
+            case "add an employee": addEmployee();
+                break;
+
+            case "update an employee role": updateEmployee();
+                break;
+
+            default: console.log("There was nothing selected");
+        }
+    })
+}
+
+function allDepartments() {
+    // I am presented with a formatted table showing department names and department ids
+}
+
+function allRoles() {
+    // I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+}
+
+function allEmployees() {
+    // I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+}
+
+function addDepartment() {
+    // I am prompted to enter the name of the department and that department is added to the database
+}
+
+function addRole() {
+    // I am prompted to enter the name, salary, and department for the role and that role is added to the database
+}
+
+function addEmployee() {
+    // I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+}
+
+function updateEmployee() {
+    // I am prompted to select an employee to update and their new role and this information is updated in the database
+}
