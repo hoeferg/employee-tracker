11f8999b6b9927e57311b7d84af364c780499e58
hoeferg <111393796+hoeferg@users.noreply.github.com>
2022-10-19 09:01:52 -0700

    added prompts to add functions. cleaned code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const db = require('./db/connection')
 const { default: inquirer } = require('inquirer');
 const mysql = require('mysql2');
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
@@ -11,8 +12,12 @@ const db = mysql.createConnection(
         password: 'yourpassword',
         database: 'employees_db'
     },
-    console.log(`Connected to the books_db database.`)
-);
+
+    connection.connect(err => {
+        if (err) throw err;
+        console.log(`Connected to the books_db database.`)
+    }))
+
 
 
 
@@ -22,7 +27,11 @@ function startQ() {
             type: 'list',
             name: 'startQ',
             message: 'What would you like to do?',
-            choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+            choices: ["view all departments",
+                "view all roles",
+                "view all employees", "add a department",
+                "add a role",
+                "add an employee", "update an employee role"]
         }
     ]).then(function (userInput) {
         switch (userInput.startQ) {
@@ -56,48 +65,100 @@ function allDepartments() {
     // I am presented with a formatted table showing department names and department ids
     db.query('SELECT * FROM department', function (err, results) {
         console.table(results);
-    })
-};
+    }
+    )
+}
+
 
 function allRoles() {
     // I am presented with the job title, role id, the department that role belongs to, and the salary for that role
     db.query('SELECT * FROM role', function (err, results) {
         console.table(results);
-    })
-};
+    }
+    )
+}
 
 function allEmployees() {
     // I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
     db.query('SELECT * FROM employee', function (err, results) {
         console.table(results);
-    })
-};
+    }
+    )
+}
 
 function addDepartment() {
     // I am prompted to enter the name of the department and that department is added to the database
     inquirer.prompt([
         {
             type: 'input',
-            name: 'addDepartmentQ',
+            name: 'addDept',
             message: 'What department would you like to add?',
         }
     ]).then(answers => {
-        db.query(`INSERT INTO department (id, name) VALUE ?" ('id', 'addDepartmentQ'`,
+        db.query('INSERT INTO department (name) VALUE (?)',
             function (err, results) {
                 console.log(results);
+
+                allDepartments();
             })
     })
-};
+}
 
 
 function addRole() {
     // I am prompted to enter the name, salary, and department for the role and that role is added to the database
-};
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addDept',
+            message: 'What department would you like to add?',
+        }
+    ]).then(answers => {
+        db.query('INSERT INTO department (name) VALUE (?)',
+            function (err, results) {
+                console.log(results);
+
+                allDepartments();
+            })
+    })
+}
 
 function addEmployee() {
     // I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-};
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'addEmp',
+            message: 'What employee would you like to add?',
+        }
+    ]).then(answers => {
+        db.query('INSERT INTO employee (name) VALUE (?)',
+            function (err, results) {
+                console.log(results);
+
+                allEmployees();
+            })
+    })
+    // TODO: add more questions to add all information about the employee
+}
 
 function updateEmployee() {
     // I am prompted to select an employee to update and their new role and this information is updated in the database
-};
+    const employeeList = `SELECT first_name, last_name FROM employee`
+
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'updateEmp',
+            message: 'Which employee would you like to update?',
+            choices: employeeList
+        }
+    ]).then(answers => {
+        db.query('INSERT INTO employee (name) VALUE (?)',
+            function (err, results) {
+                console.log(results);
+
+                allDepartments();
+            })
+    })
+}
