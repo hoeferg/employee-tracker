15fa079f7fad6f8e6cde922d026db05c1cc96543
Gayle Hoefer <gaylehoefer@gmail.com>
2022-10-19 20:49:03 -0700

    edited roles of seeds folder

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,14 +2,13 @@ INSERT INTO department (id, name)
 VALUES (1, "HR"),
         (2, "Engineer"),
         (3, "Manager"),
-        (4, "Intern"),
-        (5, "Sales Associate");
+        (4, "Intern");
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Manager", 150000, 1),
+VALUES (1, "HR", 150000, 1),
         (2, "Enginner" 200000, 2),
-        (3, "Intern" 50000, 3),
-        (4, "HR" 150000, 4);
+        (3, "Manager" 50000, 3),
+        (4, "Intern" 150000, 4);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (1, "John" "Smith", 1, 1),
